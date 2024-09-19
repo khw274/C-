9f0ceb652276f88c086c66afb0943e724d9c2a4d
김현우 <125671828+khw274@users.noreply.github.com>
2024-09-19 16:05:59 +0900

    Update 스타크래프트.cpp

--- 모두의 코드/스타크래프트.cpp
+++ 모두의 코드/스타크래프트.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
@@ -7,30 +8,48 @@ class Marine {
 	// 멤버 변수
 	// 객체의 내부적 성질 및 상태: private
 	int hp;								// 체력
-	int coord_x, coord_y;						// 위치
+	int coord_x, coord_y;				// 위치
 	int damage;							// 공격력
-	bool is_dead;							// 사망
+	bool is_dead;						// 사망
+	char* name;							// 마린 이름, 동적 할당
 
 public:
 	
 	// 생성자, 멤버 함수
 	Marine();							// 디폴트 생성자
-	Marine(int x, int y);						// x, y 좌표에 마린 생성
+	Marine(int x, int y);				// x, y 좌표에 마린 생성
+	Marine(int x, int y, const char* marine_name);  // 이름 지정, const는 객체가 변경되지 않도록 함 
 	
-	// 멤버 함수
-	int attack();							// 데미지 리턴
-	void be_attacked(int damage_earn);				// 입는 데미지
-	void move(int x, int y);					// 새로운 위치
-	
+	~Marine();							// 소멸자
 
-	void show_stat();						// 상태 
+	// 멤버 함수
+	int attack();						// 데미지 리턴
+	void be_attacked(int damage_earn);	// 입는 데미지
+	void move(int x, int y);			// 새로운 위치
+	
+	void show_stat();					// 상태 
 };
 
 // 여기서부터 객체 변수(인스턴스 변수)
-Marine::Marine() {  							// Marine 클래스의 객체 생성
+Marine::Marine() {  // Marine 클래스의 객체 생성
 	// 디폴트 생성자 초기화
 	hp = 50;
 	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y, const char* marine_name) {
+	name = new char[strlen(marine_name) + 1];  // 멤버 변수 동적 할당
+	// strlen: 문자열의 길이 반환, +1을 해준 이유는 문자열 끝에 null 문자 \n을 추가하기 위해
+	// new 연산자는 동적 메모리 할당, 해당 메모리 주소를 name 포인터에 저장
+	//strcpy(name, marine_name);  // 전달된 marine_name을 name 변수에 복사
+	strcpy_s(name, strlen(marine_name) + 1, marine_name);
+
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
 	damage = 5;
 	is_dead = false;
 }
@@ -42,6 +61,7 @@ Marine::Marine(int x, int y) {
 	hp = 50;
 	damage = 5;
 	is_dead = false;
+	name = NULL;
 }
 
 int Marine::attack() {
@@ -61,23 +81,31 @@ void Marine::move(int x, int y) {
 }
 
 void Marine::show_stat() {
-	cout << " *** Marine *** " << '\n';
+	cout << " *** Marine : " << name << " ***" << '\n';
 	cout << " Location: ( " << coord_x << " , " << coord_y << " ) " << '\n';
 	cout << " HP : " << hp << '\n';
 }
 
-int main() {
-	Marine* marines[100];
+Marine::~Marine() {
+	cout << name << " 의 소멸자 호출" << "\n";
 
-	marines[0] = new Marine(2, 3);  // new와 malloc의 차이: new는 생성자도 만들어줌
-	marines[1] = new Marine(3, 5);
+	if (name != NULL) {  // 동적으로 할당이 되었을 경우에
+		delete[] name;   // name 자체가 char의 배열로 동적할당 되었기 때문에 delete[]
+	}
+}
+
+int main() {
+	Marine* marines[100];  
+
+	marines[0] = new Marine(2, 3, "Marine 1");  // new와 malloc의 차이: new는 생성자도 만들어줌
+	marines[1] = new Marine(3, 5, "Marine 2");
 
 	marines[0]->show_stat();
 	marines[1]->show_stat();
 
 	cout << "마린 1이 마린 2 공격" << '\n';
 
-	marines[0]->be_attacked(marines[1]->attack());
+	marines[1]->be_attacked(marines[0]->attack());
 
 	marines[0]->show_stat();
 	marines[1]->show_stat();
@@ -85,6 +113,4 @@ int main() {
 	// 동적 메모리 해제
 	delete marines[0];
 	delete marines[1];
-
-
 }
