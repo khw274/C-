23fe9ba6fd3ba4ed2551171e81c5ff108753e30f
김현우 <125671828+khw274@users.noreply.github.com>
2024-09-19 16:48:58 +0900

    Update 소멸자 예시.cpp

--- 모두의 코드/소멸자 예시.cpp
+++ 모두의 코드/소멸자 예시.cpp
@@ -4,7 +4,7 @@
 class Test {
 	char c;				// 멤버 변수
 public:
-	Test(char _c) {		// 멤버 함수, 생성자
+	Test(char _c) {			// 멤버 함수, 생성자
 		c = _c;
 		std::cout << "생성자호출" << c << std::endl;
 	}
@@ -13,12 +13,12 @@ public:
 	}
 };
 
-void simple_function() { // 지역 함수
-	Test b('b');  // class Test의 객체 b 생성
+void simple_function() { 		// 지역 함수
+	Test b('b');  			// class Test의 객체 b 생성
 }
 
 int main() {
-	Test a('a');  // class Test의 객체 a 생성
+	Test a('a');  			// class Test의 객체 a 생성
 
-	simple_function();  // 여기서 생성된 객체 b는 지역 객체기 때문에 simple_function()이 종료됨과 동시에 소멸
+	simple_function();  		// 여기서 생성된 객체 b는 지역 객체기 때문에 simple_function()이 종료됨과 동시에 소멸
 }
