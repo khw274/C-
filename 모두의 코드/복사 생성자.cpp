51b3617a5504968e3f24109282784b7d97c65ff2
김현우 <125671828+khw274@users.noreply.github.com>
2024-09-20 11:49:20 +0900

    Update 복사 생성자.cpp

--- 모두의 코드/복사 생성자.cpp
+++ 모두의 코드/복사 생성자.cpp
@@ -16,8 +16,8 @@ public:  // 멤버 함수, 생성자
 };
 
 Photon_Cannon::Photon_Cannon(const Photon_Cannon& pc) {  // 복사 생성자, Photon_Cannon의 객체 pc를 상수 레퍼런스로 받음
-														 // const 이기 때문에 pc의 데이터를 변경할 수 없음
-														 // 복사 생성자 내부에서 데이터 변경 불가, 새롭게 초기화 되는 인스턴스 변수에게 복사만 가능
+							 // const 이기 때문에 pc의 데이터를 변경할 수 없음
+							 // 복사 생성자 내부에서 데이터 변경 불가, 새롭게 초기화 되는 인스턴스 변수에게 복사만 가능
 	cout << "복사 생성자 호출!" << "\n";
 
 	// 인스턴스 변수
@@ -48,7 +48,7 @@ int main() {
 	Photon_Cannon pc1(3, 3);  // pc1 객체 생성, 생성자 오버로딩
 	Photon_Cannon pc2(pc1);   // 인자 pc1을 넘겨 복사 생성자 호출
 	Photon_Cannon pc3 = pc2;  // 인자 pc2를 넘겨 pc2 복사 생성자 호출
-							  // =  Photon_Cannon pc3(pc2);
+				  // =  Photon_Cannon pc3(pc2);
 
 	// 복사 생성자는 오직 '생성' 시에만 호출
 
